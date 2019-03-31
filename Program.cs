b3c33abf81a73f78494df70d3901464d62152f3d
Andy Willis <andy.willis@live.com>
2019-03-31 17:12:31 -0500

    Add generated project

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace manpower
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
