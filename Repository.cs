7cfa7b5e23f6f3f2d342a16904b25ee5805d3e75
GANESH KUMAR <ganesh@synct.ag>
2020-06-19 14:52:03 +0530

    Code challange completion committed...

---
+++ Repository.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace manpower
+{
+    public class Repository : RepositoryBase
+    {
+        public Repository(FileLocations fileLocations, string configPath) : base(fileLocations, configPath)
+        {
+ 
+        }
+
+        public override void SaveAssignments(IEnumerable<Assignment> assignments, string saveToPath)
+        {
+            string filePath = base.GetPath(saveToPath);
+            var csv = new StringBuilder();
+            var headerLine = string.Format("{0},{1},{2}", "TaskId", "PersonId", "Day"); 
+            csv.AppendLine(headerLine);
+            foreach (var assignment in assignments.OrderBy(x => x.Day)) //Dispaly result order by Day for see how many days will take for whole tasks
+            {
+                var newLine = string.Format("{0},{1},{2}", assignment.Task.Id, assignment.Person.Id,assignment.Day);
+                csv.AppendLine(newLine);   
+            }
+            
+            File.WriteAllText(filePath, csv.ToString());
+        }
+    }
+}
---
+++ TaskPlanner.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace manpower
+{
+    public class TaskPlanner : ITaskPlanner
+    {
+        IRepository _repository;
+        public TaskPlanner(IRepository repository)
+        {
+            _repository = repository;
+        }
+
+        public IEnumerable<Assignment> Execute()
+        {
+            List<Assignment> assignments = new List<Assignment>();
+            int day = 1;
+            foreach(var task in _repository.Tasks.OrderByDescending(x => x.IsPriority).ToList()) //Take priority tasks first.
+            {
+                // Get people who are all not have task in the given day.
+                IEnumerable<Person> persons = _repository.People.Where(x => !assignments.Where(y => y.Day == day).Select(y => y.Person.Id).Contains(x.Id)).ToList();
+                day = persons.Count() > 0 ? day : day++;
+                //Get assignments
+                assignments = ProcessAssignments(assignments, task, day);
+            }
+            return assignments;
+        }
+
+        private List<Assignment> ProcessAssignments(List<Assignment> assignments, Task task, int day)
+        {
+            Assignment assignment = new Assignment();
+            // Get people who are all not have task in the given day.
+            IEnumerable<Person> persons = _repository.People.Where(x => !assignments.Where(y => y.Day == day).Select(y => y.Person.Id).Contains(x.Id)).ToList();
+            assignment.Task = task;
+
+            if (persons.Count() > 0)
+            {
+                // Check the person has required skills or not
+                Person person = persons.Where(x => x.Skills.Contains(task.SkillRequired)).FirstOrDefault();
+                if (person != null)
+                {
+                    assignment.Person = person;
+                    assignment.Day = day;
+                    //Add assignment in list.
+                    assignments.Add(assignment);
+                }
+                else
+                    ProcessAssignments(assignments, task, ++day);   //Recursive function for assign task in next day if people has not available on the given day
+            }
+            else
+                ProcessAssignments(assignments, task, ++day);   ///Recursive function for assign task in next day if people has not available on the given day
+
+            return assignments;
+        }
+    }
+}
